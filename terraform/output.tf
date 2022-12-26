e830be9e248684b167a521ef87e29f043e915b03
Jazeel <48310743+jaezeu@users.noreply.github.com>
2022-12-26 13:10:05 +0800

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,8 +1,12 @@
-output "cluster_endpoint" {
+output "postgres_cluster_endpoint" {
   description = "Writer endpoint for the cluster"
   value       = module.postgresql.cluster_endpoint
 }
 
 output "private_subnets" {
   value = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
 }
